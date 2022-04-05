5e558b80af3eb9ed4fef94c1de61d0ef4d56df80
anonimo223 <anguielugo44@gmail.com>
2022-04-05 15:47:23 -0500

    Lugo: Actualizacion de la calculadora

--- Calculadora/CalculadoradeImpuestos.cs
+++ Calculadora/CalculadoradeImpuestos.cs
@@ -4,25 +4,21 @@ namespace Calculadora
 {
     public class CalculadoradeImpuestos
     {
-        //Metodo Obtener el IVA a partir del monto de una reserva. 19%
         public decimal ObtenerIVA(decimal montoReserva)
         {
             const decimal porcentaje = 0.19m;
-            //return 0;
-            //decimal ivaReserva = porcentaje * montoReserva;
-            //return ivaReserva;
             return porcentaje * montoReserva;
         }
 
-        //Metodo Obtener el impuesto estatal sobre hospedaje a partir del monto de una reserva
         public decimal ObtenerISH(decimal montoReserva, decimal porcentaje)
         {
-            //decimal ishReserva = montoReserva * porcentaje;
-            //return 0;
-            //decimal ivaReserva = porcentaje * montoReserva;
-            //return ivaReserva;
-            //return ishReserva;
             return montoReserva * porcentaje;
+        }
+
+        public decimal ObtenerMontoTotal(decimal montoReserva, int tiempo)
+        {
+            const decimal capital = 2000;
+            return capital * montoReserva * tiempo;
         }
     }
 }
--- CalculadoradeImpuestosTest/CalculadoraTest.cs
+++ CalculadoradeImpuestosTest/CalculadoraTest.cs
@@ -1,50 +1,40 @@
 using Calculadora;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CalculadoradeImpuestosTest
+namespace CalculadoradeImpuestosTest 
+{ 
 
-    /* 
-        ** Obtener el IVA a partir del monto de una reserva. 19%
-        ** Obtener el impuesto estatal sobre hospedaje a partir del monto de una reserva
-        *  Obtener el monto total de la reserva (con impuestos incluidos)
-     */
-{
     [TestClass]
     public class CalculadoraTest
     {
-        //Test Obtener el IVA a partir del monto de una reserva. 19%
         [TestMethod]
         public void ImpuestosTest()
         {
-            //Arrange
             CalculadoradeImpuestos ci = new CalculadoradeImpuestos();
 
-            //Act
             decimal iva = ci.ObtenerIVA(1550.00m);
 
-            //Assert
-            // iva = 19%
-            // monto reserva = 1550
-            //1550.00 * .19 = 294.5
             Assert.AreEqual(294.5m, iva);
         }
 
-
-        // Test Obtener el impuesto estatal sobre hospedaje a partir del monto de una reserva
         [TestMethod]
         public void ISHTest()
         {
-            //Arrange
             CalculadoradeImpuestos ci = new CalculadoradeImpuestos();
 
-            //Act
             decimal ish = ci.ObtenerISH(1550.00m, 0.03m);
 
-            //Assert
-            // ish = 3%
-            // monto reserva = 1550
-            //1550.00 * .03 = 46.5
             Assert.AreEqual(46.5m, ish);
+        }
+
+        [TestMethod]
+        public void MTR()
+        {
+            CalculadoradeImpuestos ci = new CalculadoradeImpuestos();
+
+            decimal mtr = ci.ObtenerMontoTotal(1550.00m, 5);
+
+            Assert.AreEqual(155000000m, mtr);
         }
     }
 }
